4f1adacd02b814c054183578314c6c920c521daa
Alessandra Silva <alessandra.apds@gmail.com>
2023-03-30 18:38:25 +0200

    first improvements with css

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import JobsData from "./JobsData";
+import "./Components.css";
 
 const Form = () => {
   const [input, setInput] = useState("");
@@ -13,15 +14,22 @@ const Form = () => {
 
   return (
     <div class="container">
-      <div>Find your next job</div>
+      <i class="bi bi-briefcase-fill"></i>
+      <h1>Find your next job</h1>
       <form>
         <input
           type="text"
           placeholder="Enter a job title or location"
+          class="input-form"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <input type="submit" value="Find me a job" onClick={saveInput} />
+        <input
+          type="submit"
+          class="form-button"
+          value="Find me a job"
+          onClick={saveInput}
+        />
       </form>
       <JobsData keyword={keyword} />
     </div>
